c037f75e0b2de6058e11146a41b11e4b222c7c7d
jguerrero104 <jguerrero110761@gmail.com>
2024-11-16 03:38:10 -0600

    api url change

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Ensure this matches your server URL
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api', // Fallback to localhost for development
 });
 
 export default api;
